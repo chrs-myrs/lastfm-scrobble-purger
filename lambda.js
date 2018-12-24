848df85480cdb0059bb192cb58005d266255a6b2
Chris Myers <chris@myrs.uk>
2018-12-24 07:14:08 +0000

    lambda refactor to use promises

--- lambda.js
+++ lambda.js
@@ -9,16 +9,12 @@ AWS.config.update({ region: 'eu-west-2' })
 
 // Create an SQS service object
 var sqs = new AWS.SQS({ apiVersion: '2012-11-05' })
-
 var cw = new AWS.CloudWatch({ apiVersion: '2010-08-01' })
 
-const yaml = require('js-yaml');
+
+
+const yaml = require('js-yaml')
 const fs = require('fs')
-const config = yaml.safeLoad(fs.readFileSync('config.yml', 'utf8'));
-
-
-if (process.env.hasOwnProperty('config'))
-  Object.assign(config, JSON.parse(process.env.config))
 
 if (!process.env.hasOwnProperty('sqs_url')) {
   console.error("Environment variable 'sqs_url' missing.")
@@ -27,6 +23,42 @@ if (!process.env.hasOwnProperty('sqs_url')) {
 
 const sqs_url = process.env.sqs_url
 
+function getConfigPromise() {
+  return getConfigFileAsStringPromise().then(data => {
+    var config = yaml.safeLoad(data)
+    if (process.env.hasOwnProperty('config'))
+      Object.assign(config, JSON.parse(process.env.config))
+    return config
+  })
+}
+
+function getConfigFileAsStringPromise() {
+  if (process.env.hasOwnProperty('s3_config_url')) {
+    
+    const s3 = new AWS.S3();
+    var url = require('url').parse(process.env.s3_config_url)
+    console.log("Loading config file from: "+url.href)
+
+    let protocol = url.protocol.replace(':', '')
+    if (protocol !== "s3") throw new Error("Protocol " + protocol + " not supported. [Supported: s3]")
+
+    let params = {
+      Bucket: url.hostname,
+      Key: url.pathname.substr(1)
+    }
+
+    return s3.getObject(params).promise().then(res => res.Body.toString()).catch(err => {throw err})
+
+  } else {
+    return new Promise(function (resolve, reject) {
+      fs.readFile('config.yml', 'utf8', (err, data) => {
+        err ? reject(err) : resolve(data)
+      });
+    })
+  }
+}
+
+
 /**
  * @typedef {Object} env - creates a new type named 'SpecialType'
  * @property {string} sqs_url - a number property of SpecialType
@@ -34,50 +66,57 @@ const sqs_url = process.env.sqs_url
 
 exports.crawler = (event, context, callback) => {
 
-  config.startPage = 1
-  config.endPage = config.pages || 1
+  getConfigPromise().then(config => {
+    config.startPage = 1
+    config.endPage = config.pages || 1
 
-  const crawler = require('./crawler')
-  crawler.crawl(config).then(
-    tracks => queueTracks(tracks)
-  ).then(res => {
-    let tracksQueued = res.reduce((a,b) => a + b, 0)
-    
-    callback(null, 'Queued ' + tracksQueued + ' tracks on SQS.')
-    logMetric('Queued', tracksQueued)
-  }).catch(err => {
-    callback(err)
+    const crawler = require('./crawler')
+    crawler.crawl(config).then(
+      tracks => queueTracks(tracks)
+    ).then(res => {
+      let tracksQueued = res.reduce((a, b) => a + b, 0)
+
+      callback(null, 'Queued ' + tracksQueued + ' tracks on SQS.')
+      logMetric('Queued', tracksQueued)
+    }).catch(err => {
+      callback(err)
+    })
   })
+
+
 };
 
 exports.unscrobbler = (event, context, callback) => {
-  getMessages(Math.min(config.maxUnscrobbleBatchSize, 10)).then(msgs => {
-    if (!msgs.length) {
-      console.log("No messages on queue")
-      logMetric("Unscrobbled", 0)
-      return 0;
-    }
+  getConfigPromise().then(config => {
+    getMessages(Math.min(config.maxUnscrobbleBatchSize, 10)).then(msgs => {
+      if (!msgs.length) {
+        console.log("No messages on queue")
+        logMetric("Unscrobbled", 0)
+        return 0;
+      }
 
-    console.log(`Fetched ${msgs.length} messages`)
+      console.log(`Fetched ${msgs.length} messages`)
 
-    try {
-      let tracksToUnscrobble = msgs.map(msg => Object.assign({ msgId: msg.ReceiptHandle }, JSON.parse(msg.Body)))
-      require('./unscrobble').unscrobble(config, tracksToUnscrobble, track => {
-        deleteMessage(track.msgId)
-      }, (error, track) => {
-        // log already sent to console                     
-      }).then((ret) => {
-        let successes = ret.reduce((a, b) => a + b, 0)
-        logMetric("Unscrobbled", successes)
-        logMetric("Unscrobble Fails", ret.length - successes)
-        console.log("Finished with " + ret.reduce((a, b) => a + b, 0) + "/" + ret.length + " sucesses")
-      }).catch(err => console.log(err))
-    } catch (err) {
-      console.log(err)
-    }
+      try {
+        let tracksToUnscrobble = msgs.map(msg => Object.assign({ msgId: msg.ReceiptHandle }, JSON.parse(msg.Body)))
+        require('./unscrobble').unscrobble(config, tracksToUnscrobble, track => {
+          deleteMessage(track.msgId)
+        }, (error, track) => {
+          // log already sent to console                     
+        }).then((ret) => {
+          let successes = ret.reduce((a, b) => a + b, 0)
+          logMetric("Unscrobbled", successes)
+          logMetric("Unscrobble Fails", ret.length - successes)
+          console.log("Finished with " + ret.reduce((a, b) => a + b, 0) + "/" + ret.length + " sucesses")
+        }).catch(err => console.log(err))
+      } catch (err) {
+        console.log(err)
+      }
 
 
+    })
   })
+
 }
 
 exports.unscrobbleTrigger = (event, context, callback) => {
@@ -165,7 +204,7 @@ function queueTracks(tracks) {
           console.log("Error", err);
           reject(err)
         } else {
-          console.log("Tracks successfully queued, message ids: "+data.Successful.map(res => res.Id).join(', '))
+          console.log("Tracks successfully queued, message ids: " + data.Successful.map(res => res.Id).join(', '))
           resolve(data.Successful.length)
         }
       })
